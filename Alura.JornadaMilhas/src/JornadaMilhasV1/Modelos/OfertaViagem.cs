8b7cfb10682088c73defc93436d7c543522b83a8
Marcos Paulo <marcos.poo@gmail.com>
2024-03-05 22:12:10 -0300

    Aula 3 - TDD - Test-Driven Development
    Nessa aula, você aprendeu como:
    - Criar testes de unidade utilizando o TDD, que consiste em criar o teste primeiro e depois fazer as alterações necessárias no código para que o teste passe;
    - Refatorar o código para aplicar a melhor implementação e assim ter um código conciso, limpo e otimizado.

--- Alura.JornadaMilhas/src/JornadaMilhasV1/Modelos/OfertaViagem.cs
+++ Alura.JornadaMilhas/src/JornadaMilhasV1/Modelos/OfertaViagem.cs
@@ -1,19 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using JornadaMilhasV1.Validador;
+﻿using JornadaMilhasV1.Validador;
 
 namespace JornadaMilhasV1.Modelos;
 
 public class OfertaViagem: Valida
 {
+    public const double DESCONTO_MAXIMO = 0.7;
+    private double desconto;
+    
     public int Id { get; set; }
     public Rota Rota { get; set; } 
     public Periodo Periodo { get; set; }
     public double Preco { get; set; }
 
+    public double Desconto
+    {
+        get => desconto;
+        set
+        {
+            desconto = value;
+            if (desconto >= Preco)
+            {
+                Preco *= (1 - DESCONTO_MAXIMO);
+            }
+            else if (desconto > 0)
+            {
+                Preco -= desconto;
+            }
+        }
+    }
 
     public OfertaViagem(Rota rota, Periodo periodo, double preco)
     {
---
+++ Alura.JornadaMilhas/tests/JornadaMilhas.Test/OfertaViagemDesconto.cs
@@ -0,0 +1,62 @@
+﻿using JornadaMilhasV1.Modelos;
+
+namespace JornadaMilhas.Test
+{
+    public class OfertaViagemDesconto
+    {
+        [Fact]
+        public void RetornaPrecoAtualizadoQuandoAplicadoDesconto()
+        {
+            //Cenário - Arrange
+            Rota rota = new("Fortalza", "Gramado");
+            Periodo periodo = new(DateTime.Parse("2024-05-01"), DateTime.Parse("2024-05-31"));
+            double precoOriginal = 10000.00;
+            double desconto = 2000.00;
+            double precoComDesconto = precoOriginal - desconto;
+            OfertaViagem oferta = new(rota, periodo, precoOriginal);
+
+            //Ação - Act
+            oferta.Desconto = desconto;
+
+            //Validação - Assert
+            Assert.Equal(precoComDesconto, oferta.Preco);
+        }
+
+        [Fact]
+        public void RetornaDescontoMaximoQuandoValorDescontoMaiorQuePreco()
+        {
+            //Cenário - Arrange
+            Rota rota = new("Fortalza", "Gramado");
+            Periodo periodo = new(DateTime.Parse("2024-05-01"), DateTime.Parse("2024-05-31"));
+            double precoOriginal = 100.00;
+            double desconto = 120.00;
+            double precoComDesconto = 30.00;
+            OfertaViagem oferta = new(rota, periodo, precoOriginal);
+
+            //Ação - Act
+            oferta.Desconto = desconto;
+
+            //Validação - Assert
+            Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
+        }
+
+        [Fact]
+        public void RetornaPrecoOriginalQuandoValorDescontoNegativo()
+        {
+            //arrange
+            Rota rota = new Rota("Fortalza", "Rio de Janeiro");
+            Periodo periodo = new Periodo(new DateTime(2024, 05, 01), new DateTime(2024, 05, 10));
+            double precoOriginal = 100.00;
+            double desconto = -20.00;
+            double precoComDesconto = precoOriginal;
+            OfertaViagem oferta = new OfertaViagem(rota, periodo, precoOriginal);
+
+            //act
+            oferta.Desconto = desconto;
+
+            //assert
+            Assert.Equal(precoComDesconto, oferta.Preco, 0.001);
+        }
+
+    }
+}
